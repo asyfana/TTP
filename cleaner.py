a60b0667396f0d6661f30350e89add991aa21232
asyfana <108052294+asyfana@users.noreply.github.com>
2024-11-05 14:34:45 +0800

    Add files via upload

---
+++ cleaner.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from utils import display_footer
+
+def cleaner_page():
+    st.title("Cleaner Dashboard")
+    st.write("This is the page where cleaners can manage their tasks.")
+    # Add additional functionality such as viewing assigned jobs, updating job status, etc.
+
+display_footer()
